b6dc2224bb4d8aa482dfcc958fd8ea9eb7ed4e43
metromap <testemail@test.cz>
2018-01-23 23:05:46 +0100

    remove comments from stationcombobox

--- src/components/StationCombobox.jsx
+++ src/components/StationCombobox.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 
 class StationCombobox extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   var {stationName} = this.props;
-  //   this.state = {value: stationName};
-  // }
   handleChange(event) {
-    //console.log('handle change');
-    //this.setState({value: event.target.value});
-    //console.log(event.target.value);
     this.props.onChangeSelection(event.target.value);
   }
   render() {
@@ -20,10 +12,7 @@ class StationCombobox extends Component {
             <option value="Empty combobox">Empty combobox</option>
           );
         }
-        // console.log('!!!stationslist!!!');
-        // console.log(stationList);
         return stationList.map((element) => {
-            //console.log(element);
             return (
               <option key={element.name} value={element.name}>{element.name}</option>
             );
